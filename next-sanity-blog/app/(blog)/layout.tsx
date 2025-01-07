087398736dcb381bbf26ad06c6ceb80b11f6d620
Suleman Roonjha <36152719+roonjha@users.noreply.github.com>
2025-01-08 00:43:19 +0500

    update comit

--- next-sanity-blog/app/(blog)/layout.tsx
+++ next-sanity-blog/app/(blog)/layout.tsx
@@ -81,8 +81,9 @@ export default async function RootLayout({
               ) : (
                 <div className="flex flex-col items-center py-28 lg:flex-row">
                   <h3 className="mb-10 text-center text-4xl font-bold leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-5xl">
-                  "Space Insights, Uniquely Yours"
+                  &quot;Space Insights, Uniquely Yours&quot;
                   </h3>
+
                   <div className="flex flex-col items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
                     <Link
                       href="/"
